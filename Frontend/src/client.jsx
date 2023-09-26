d93977b7dfa093d22b861fb48d625a43f5c88a4d
Chakshu Shaktawat <93027256+Chakshu061@users.noreply.github.com>
2023-09-26 18:17:38 +0530

    token

--- Frontend/src/client.jsx
+++ Frontend/src/client.jsx
@@ -2,11 +2,11 @@ import {createClient} from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
+  projectId: 's5y4ny91',
   dataset: 'production',
   apiVersion: '2023-04-02',
   useCdn: true,
-  token: process.env.SANITY_TOKEN,
+  token: 'sk2l1GU7XZxZI5VOJ670n0d8b6Y2ynHpbkjegGZIocPgb5tS26AeLv3msyis96jruz90uWNWcdZ6bDkhQdai1f8eoI4FSNRAgU4QS9TspyzmjURFBsSks0Ynq3faQBpVWAKDHDOap39vqyCb4wBG70dTaWp8rDYmHyauWk6pzlOsCmbdDcqm',
 });
   
 const builder = imageUrlBuilder(client);
