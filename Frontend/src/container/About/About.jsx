55afbdf26229cd5ec5e6e50aeda0a0c2dd1ce5c7
Chakshu Shaktawat <93027256+Chakshu061@users.noreply.github.com>
2023-09-26 18:13:10 +0530

    added resume

--- Frontend/src/container/About/About.jsx
+++ Frontend/src/container/About/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
       <div className="app__profiles">
         <p>Hi there! I'm Chakshu, a developer and designer with a passion for using technology to make a positive impact on the community. I'm committed to creating functional and visually appealing designs that prioritize the user experience. Although I'm new to the field, I'm dedicated to continuous learning and staying up-to-date with the latest trends and advancements. Thank you for stopping by my portfolio, and I look forward to working on projects that make a meaningful difference in the community.</p>
       </div>
-      <button><a href="https://drive.google.com/file/d/1N36CVwaciVoixIT04cpf8aJp2hLTAsT0/view?usp=sharing" className="button-re">Link to my Resume</a></button>
+      <button><a href="https://drive.google.com/file/d/1541E7RyNQ7D_VFU1zZKL1hHREDcytd1-/view?usp=sharing" className="button-re">Link to my Resume</a></button>
     </>
   );
 };
